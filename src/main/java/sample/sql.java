e1124a6bbb57cd5b964f977333dfafad6a65bd05
hrtsgym <hrt_a_1206@yahoo.co.jp>
2025-01-03 02:01:51 +0900

    '更新・削除の追加'

--- src/main/java/sample/sql.java
+++ src/main/java/sample/sql.java
@@ -24,15 +24,17 @@ public class sql {
 			// データベース接続
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/to_do_app", "root", "Merenn1ninaru");
 			con.setAutoCommit(false);
+
 			// SQL実行準備
 			stmt = con.createStatement();
-			
-			int inscnt = 0;
-			// 実行結果取得
-			inscnt = stmt.executeUpdate("INSERT INTO todolistapp VALUES('0001','Test0',false,'2025-01-01','2025-01-01','2025-01-01')");
-			System.out.println(inscnt+"行挿入されました");
-			inscnt = stmt.executeUpdate("INSERT INTO todolistapp VALUES('0002','Test1',false,'2025-01-02','2025-01-01','2025-01-01')");
-			System.out.println(inscnt+"行挿入されました");
+			//データベース登録
+			insertDate(con, stmt);
+
+			//データベース更新
+			updateDate(con, stmt);
+
+			//データベース削除
+			deleteDate(con, stmt);
 
 			con.commit();
 		} catch (SQLException e) {
@@ -53,4 +55,36 @@ public class sql {
 		}
 	}
 
+	private static void insertDate(Connection conection, Statement stmt) throws SQLException {
+		int inscnt = 0;
+		// 実行結果取得
+		inscnt = stmt.executeUpdate(
+				"INSERT INTO todolistapp VALUES('0001','Test0',false,'2025-01-01','2025-01-01','2025-01-01')");
+		System.out.println(inscnt + "行挿入されました");
+		inscnt = stmt.executeUpdate(
+				"INSERT INTO todolistapp VALUES('0002','Test1',false,'2025-01-02','2025-01-01','2025-01-01')");
+		System.out.println(inscnt + "行挿入されました");
+	}
+
+	private static void updateDate(Connection conection, Statement stmt) throws SQLException {
+		int inscnt = 0;
+		// 実行結果取得
+		inscnt = stmt.executeUpdate(
+				"UPDATE todolistapp SET is_completed = true where id = '1'");
+		System.out.println(inscnt + "行更新されました");
+		inscnt = stmt.executeUpdate(
+				"UPDATE todolistapp SET due_date = '2025-01-03' where id = '2'");
+		System.out.println(inscnt + "行更新されました");
+	}
+
+	private static void deleteDate(Connection conection, Statement stmt) throws SQLException {
+		int inscnt = 0;
+		// 実行結果取得
+		inscnt = stmt.executeUpdate(
+				"DELETE FROM todolistapp WHERE id = '1'");
+		System.out.println(inscnt + "行削除されました");
+		inscnt = stmt.executeUpdate(
+				"DELETE FROM todolistapp WHERE id = '2'");
+		System.out.println(inscnt + "行削除されました");
+	}
 }
